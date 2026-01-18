b4b2108137025252b1067f4c2d67b8ee393654d2
hs2041 <hrdpsingh12@gamil.com>
2026-01-18 12:30:58 +0530

    Added encoder and decoder

--- transformer.py
+++ transformer.py
@@ -6,3 +6,19 @@ with open('input.txt', 'r', encoding='utf-8') as f:
 
 print("Length of dataset: ", len(text))
 
+# List of unique characters in the text
+
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+print(''.join(chars))
+print(vocab_size)
+
+# Create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars)}
+itos = { i:ch for i,ch in enumerate(chars)}
+
+encode = lambda s: [stoi[c] for c in s] # takes a string and outputs a list of integer
+decode = lambda l: ''.join([itos[i] for i in l]) # coverts integers to strings
+
+print(encode("hii there"))
+print(decode(encode("hii there")))
