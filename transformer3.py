f06b60e6ce776d155d29fc0fe41d3f828c707617
hs2041 <hrdpsingh12@gamil.com>
2026-02-15 01:44:21 +0530

    GPT2 text generation

--- transformer3.py
+++ transformer3.py
@@ -1,10 +1,24 @@
+# Load GPT 2 model and it's weights and run inference promtps
+
 import torch
+
 import torch.nn as nn
 from torch.nn import functional as F
 from transformers import GPT2LMHeadModel
+import matplotlib as plt
+from transformers import pipeline, set_seed
 
 model_hf = GPT2LMHeadModel.from_pretrained("gpt2") #124M param model
 sd_hf = model_hf.state_dict()
 
-for k, v in sd_hf.items():
-    print(k, v.shape)
+# 50247 tokens in gpt2, Each token is represented by an embedding of vector (size 768)
+# for k, v in sd_hf.items():
+#     print(k, v.shape)
+
+generator = pipeline('text-generation', model = 'gpt2')
+set_seed(42)
+generated_txt = generator("But the problem is", max_length = 30, num_return_sequences = 5, truncation = True)
+
+for generated in generated_txt:
+    print(generated)
+# print(generated_txt)
